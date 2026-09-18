78864028eed7bc1438b093777b6466fdfb185768
witold-k <85440409+witold-k@users.noreply.github.com>
2026-09-18 19:09:29 +0200

    test: move pathutils tests to integration tests

--- src/pathutils.rs
+++ src/pathutils.rs
@@ -56,43 +56,6 @@ pub fn normalize_path(p: &Path) -> PathBuf {
     out
 }
 
-#[cfg(test)]
-mod normalize_path_tests {
-    use super::normalize_path;
-    use std::path::{Path, PathBuf};
-
-    #[test]
-    fn keeps_relative_paths_relative() {
-        assert_eq!(
-            normalize_path(Path::new("src/./module/../lib.rs")),
-            PathBuf::from("src/lib.rs")
-        );
-    }
-
-    #[test]
-    fn preserves_leading_parent_components() {
-        assert_eq!(
-            normalize_path(Path::new("../../src/../Cargo.toml")),
-            PathBuf::from("../../Cargo.toml")
-        );
-    }
-
-    #[test]
-    fn empty_and_current_directory_normalize_to_empty_relative_path() {
-        assert_eq!(normalize_path(Path::new("")), PathBuf::new());
-        assert_eq!(normalize_path(Path::new(".")), PathBuf::new());
-    }
-
-    #[cfg(unix)]
-    #[test]
-    fn absolute_paths_do_not_escape_root() {
-        assert_eq!(
-            normalize_path(Path::new("/tmp/project/../../etc/../file")),
-            PathBuf::from("/file")
-        );
-    }
-}
-
 /// Tries to flexibly find the path given by the LLM within the project directory.
 /// Allows for relative deviations, incorrect prefixes (like ../), and partial paths.
 pub fn resolve_relaxed_path<P1, P2>(projdir: P1, llm_path: P2) -> Option<PathBuf>
